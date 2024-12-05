24ee9b4f2750e0d9a0354f29cf69d25ec37cf457
April <fe6942uh@go.minnstate.edu>
2024-12-05 15:01:25 -0600

    edits to frontend

--- frontend.py
+++ frontend.py
@@ -1,17 +1,14 @@
 import backend
+#import input.py
 from PIL import Image, ImageDraw
 import io
 import random
 colblk = (0,0,0)
 colwht = (255,255,255)
 
-data = []
-with open ("input.txt", "r") as f:
-	data = f.readlines()
-for i in range(len(data)):
-	data[i] = data[i].replace('\n', '')
-while '' in data:
-	data.remove('')
+data = backend.grid_list
+length = 50
+
 
 images = []
 
@@ -21,7 +18,7 @@ images = []
 
 '''
 for i in range(20):
-	im = Image.new('RGB', (50, 50), colblk)
+	im = Image.new('RGB', (50, 50), colblk) 
 	draw = ImageDraw.Draw(im)
 	for i in range(random.randint(5,12)):
 		draw.point((random.randint(0,50),random.randint(0,50)),colwht)
@@ -30,7 +27,7 @@ for i in range(20):
 '''
 
 
-#
+'''
 for i in range(10):
 	im = Image.new('RGB', (len(data[0]), len(data)), colblk)
 	draw = ImageDraw.Draw(im)
@@ -39,16 +36,23 @@ for i in range(10):
 		for j in range(len(data[i])):
 			if stng[j] == '1':
 				draw.point((j,i), colwht)
-	draw.point((random.randint(0,5), 1), (255,0,0))
 	images.append(im)
-	
-			
+'''
+#^test cases for the gif creation function
 
 
-
-
-
-images[0].save('pillow_imagedraw.gif', 
+def creategif(data, length):
+	for f in range(length):
+		cell_data = data.return_data[f]
+		frame = Image.new('RGB', data[0].get_width, data[0].get_height, colblk)
+		draw = ImageDraw.Draw(fr)
+		for y in range(len(data[f])):
+			grid = data[f]
+			for x in grid[y]:
+				if x == '1':
+					draw.point((y,x), colwht)
+		images.append(frame)
+	images[0].save('animation.gif', 
                save_all = True, append_images = images[1:], 
-               optimize = False, duration = -1, loop = 0) 
-
+               optimize = False, duration = -1, loop = 0)
+	
